f79ad8103fed8482e6ec8cbeadd2d92b9b15b69b
Harjasilin Pradhan <harjasilin.01@gmail.com>
2026-08-21 10:33:23 +0530

    updates

---
+++ course/Demo.java
@@ -0,0 +1,12 @@
+package course;
+
+public class Demo {
+    public static void main(String[] args){
+        int x=4;
+        int y=4;
+        boolean res=x==y;
+        System.out.println(res);
+        
+        
+    }
+}
---
+++ course/Hello.java
@@ -0,0 +1,15 @@
+package course;
+
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+    int n1=7;
+    // int n2=5;
+    // int m2=n1%n2;
+
+    // n1=n1+2;
+    n1+=2;
+    System.out.println(n1);
+    }
+}
