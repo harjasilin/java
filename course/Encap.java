1b47bb2af6e4c600fc0015461f25d9a23e76dc97
Harjasilin Pradhan <harjasilin.01@gmail.com>
2026-08-23 09:55:46 +0530

    updates

---
+++ course/Encap.java
@@ -0,0 +1,35 @@
+package course;
+
+class Human {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        this.age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+}
+
+public class Encap {
+    public static void main(String a[]) {
+        Human obj1 = new Human();
+        obj1.setAge(10);
+        obj1.setName("khewjrdbsfckj");
+        ;
+
+        System.out.println(obj1.getName() + obj1.getAge());
+
+    }
+}
